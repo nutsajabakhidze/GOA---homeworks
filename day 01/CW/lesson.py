13b2cd8f0cf5971abce455c789efe020d181240b
nutsajabakhidze <nutsajabakhidze@gmail.com>
2024-09-24 15:37:31 +0400

    commit

---
+++ day 01/CW/lesson.py
@@ -0,0 +1,48 @@
+from turtle import
+ #we want to 
+
+#step 1:drew a square
+#start of square
+width(10)
+color("purple")
+speed(30)
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+#end of scuare
+
+#start of door
+left(90)
+forward(70)
+color("yellow")
+left(90)
+forward(120)
+right(90)
+forward(70)
+right(90)
+forward(120)
+#end of doors
+#start of roof
+penup()
+goto(200,200)
+pendown()
+begin_fill()
+right(150)
+color("red")
+forward(200)
+left(120)
+forward(200)
+end_fill()
+
+
+
+
+
+
+
+
+exitonclick()
---
+++ day 01/HW/home work.py
@@ -0,0 +1,35 @@
+name = "nutsa"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობისთვის მიმნიჭებელი სიმბოლო
+# "nutsa" არის ცვლადისთვის მნიშვნელობა
+
+surname = "jabakhidze"
+
+print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "nutsa"  #ეს არის str (string) ტიპის ცვლადი
+age = 12 # ეს არის int ( intager ) მთელი რიცხვი
+height = 140.0 #ეს არის float ტიპის ცვლადი ( ათწილადი )
+#boolean (bool) ტიპის ცვლადი
+
+knows_programing = True # True ან False
+is_ugly = False   #
+
+isUgli = False # ჯავასკრიპტული camelcase
+
+
+# print(name + " " + surname)
+
+# #სტრინგი არის ბრჭყალებში მოქცეული სიმბოლო
+# print(name + age)
+
+# print(type(age))
+# print(tupe(name))
+# print(tupe(surname))
+# print(tupe(height))
+# print(type(knows_programming))
+
+
+
+print(name + " " + str(age))
