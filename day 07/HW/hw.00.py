edddeedc05134d4f792e9bedd61104727162248d
nutsajabakhidze <nutsajabakhidze@gmail.com>
2024-11-13 22:44:33 +0400

    Add files via upload

---
+++ day 07/HW/hw.00.py
@@ -0,0 +1,46 @@
+
+
+#usuer1 = int(input('enter your number '))
+#user2 = int(input('enter your number '))
+#user3 = int(input('enter your number '))
+#user4 = int(input('enter your number '))
+
+#print(user1 + user2)
+#print(user3 * user4)
+
+user1 = int(input('enter your number '))
+user2 = int(input('enter your number '))
+user3 = int(input('enter your number '))
+user4 = int(input('enter your number '))
+
+print(user1 - user2)
+print(user4 / user3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
